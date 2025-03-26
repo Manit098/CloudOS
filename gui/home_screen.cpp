dc4659f799666bb98c4bba450f41cff6ef6661fc
legendary <145838047+legendryPro@users.noreply.github.com>
2025-03-26 15:20:19 +0530

    first commit

---
+++ gui/home_screen.cpp
@@ -0,0 +1,5 @@
+#include "graphics.h"
+
+void home_screen() {
+    draw_image(0, 0, "earth.bmp");  // Load and display Earth image
+}
---
+++ scripts/run_qemu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# echo "Running CloudOS in QEMU..."
+# qemu-system-x86_64 -kernel kernel.img
